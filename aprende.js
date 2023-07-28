9aec4372325c6820885abb5614fa7b33a3711472
Ruddy Rodriguez <ruddy.rodriguez.r@hotmail.com>
2023-07-28 15:17:18 -0500

    mi primer commit

---
+++ aprende.js
@@ -0,0 +1,25 @@
+// let numeroUno = 23;
+// let numeroDos = "Ruddy";
+// let Respuesta = numeroUno * numeroDos
+// alert(Respuesta);
+
+// const Numero = 30;
+// Numero = 50;
+// casa=89;
+
+//  let Direccion=prompt("Dame tu direccion: ");
+//  alert("Tu direccion es: "+Direccion)
+// let Nombre = prompt('¿Cual es tu nombre?')
+// let Edad = prompt ('¿Cual es tu edad?')
+// document.getElementById('Contenido').innerHTML=`<h2>Bienvenido ${Nombre}</h2><h3> tu edad es ${Edad}</h3>`
+
+// let nombre = prompt('¿Cual es tu nombre?')
+// let apellidos = prompt('¿Cual es tu Apellido?')
+// let empresa= prompt('¿En que empresa trabajas?')
+// let ambito= prompt('¿Cual es tu ambito laboral?')
+// let cargo= prompt('¿Que cargo tienes?')
+// let user= prompt('Dame tu usuario')
+// let contraseña = prompt('¿Cual es tu contraseña?')
+// let correo = prompt('¿Cual es tu correo?')
+
+// document.getElementById('Contenido').innerHTML=`<h1> Mucho gusto ${nombre} ${apellidos}, la empresa en la que trabajas es ${empresa}, tu ambito laboral es ${ambito}, tienes un cargo de ${cargo}, tu usuario es ${user}, tu contraseña ${contraseña}, te la verifico ${contraseña}, y correo ${correo}, eres Bienvenido!!`
